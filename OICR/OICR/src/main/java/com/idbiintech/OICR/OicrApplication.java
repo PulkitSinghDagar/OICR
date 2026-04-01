4d2f320455ada87300e5a35e0bcabf928af4ca5d
Pulkit <singhhpulkit@gmail.com>
2026-04-01 11:11:51 +0530

    firast

---
+++ OICR/OICR/src/main/java/com/idbiintech/OICR/OicrApplication.java
@@ -0,0 +1,13 @@
+package com.idbiintech.OICR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OicrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OicrApplication.class, args);
+	}
+
+}
---
+++ OICR/OICR/src/test/java/com/idbiintech/OICR/OicrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idbiintech.OICR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OicrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
